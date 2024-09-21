2bf0ab0f7b4833ca0902dc45b3e27964fd13bbb9
MarkJoaquin <markjoaquin2@gmail.com>
2024-09-20 18:35:33 -0700

    component.js and base project structure

--- src/js/App.js
+++ src/js/App.js
@@ -1,3 +1,14 @@
-export class App {
-  render() {}
+import { Component } from "./common/component.js"
+import { Header } from "./components/Header.js";
+
+export class App extends Component {
+  render() {
+    new Header(this.parentElement);
+
+    const children = $(`
+      <main></main>
+    `);
+
+    this.parentElement.append(children);
+  }
 }
--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,3 +1,12 @@
-export class Header {
-  render() {}
+import { Component } from "../common/component.js";
+export class Header extends Component {
+  render() {
+    console.log(this.parentElement);
+    const children = $(`
+      <header class="header"><h1>Fake Store</h1></header>`);
+
+      console.log(children);
+
+    this.parentElement.append(children);
+  }
 }
