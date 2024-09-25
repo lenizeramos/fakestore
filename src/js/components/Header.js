160f75523cb6b72ec0abec4364be4f69377358f7
MarkJoaquin <markjoaquin2@gmail.com>
2024-09-24 19:29:56 -0700

    Font and styling added

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -2,7 +2,7 @@ import { Component } from "../common/component.js";
 export class Header extends Component {
   render() {
     const children = $(`
-      <header class="header"><h1>Fake Store</h1></header>`);
+      <header class="header"><h1>Fake <span> <img src="https://cdn-icons-png.flaticon.com/128/80/80807.png" alt="logo"> </span>  Store</h1></header>`);
 
     this.parentElement.append(children);
   }
