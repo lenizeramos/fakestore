8a21b2190a14a7c793f9a8f6ddc2dc286619ee37
Lenize Ramos <lenizemr@gmail.com>
2025-06-01 15:28:03 -0700

    change fetch to an external API

--- src/js/components/ProductList.js
+++ src/js/components/ProductList.js
@@ -8,12 +8,23 @@ export class ProductList extends Component {
   }
   
   async getProducts() {
-    const response = await fetch("http://localhost:8000/products");
+    const response = await fetch("https://dummyjson.com/products");
     if (!response.ok) {
       throw new Error("something went wrong while getting product data");
     }
 
-    const productData = await response.json();
+    const {products } = await response.json();
+    const productData = products.filter((product) => product.images.length != 0 ).map((product) => {
+      return {
+        id: product.id,
+        title: product.title,
+        price: product.price,
+        image: product.images[0],
+        description: product.description,
+      };
+    })
+    console.log(productData, "productData");
+  
     this.setState({ productData });
   }
 
